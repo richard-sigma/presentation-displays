ae48d9cf90b78824182560551e04b4da7955f48b
richard-sigma <60799920+richard-sigma@users.noreply.github.com>
2025-10-22 07:32:23 +0800

    Update PresentationDisplaysPlugin.kt

--- android/src/main/kotlin/com/namit/presentation_displays/PresentationDisplaysPlugin.kt
+++ android/src/main/kotlin/com/namit/presentation_displays/PresentationDisplaysPlugin.kt
@@ -19,7 +19,7 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry
+//import io.flutter.plugin.common.PluginRegistry
 import org.json.JSONObject
 
 /** PresentationDisplaysPlugin */
@@ -50,19 +50,19 @@ class PresentationDisplaysPlugin : FlutterPlugin, ActivityAware, MethodChannel.M
     private const val viewTypeEventsId = "presentation_displays_plugin_events"
     private var displayManager: DisplayManager? = null
 
-    /** @hide */
-    @Suppress("unused", "DEPRECATION")
-    @JvmStatic
-    fun registerWith(registrar: PluginRegistry.Registrar) {
-      val channel = MethodChannel(registrar.messenger(), viewTypeId)
-      channel.setMethodCallHandler(PresentationDisplaysPlugin())
-
-      val eventChannel = EventChannel(registrar.messenger(), viewTypeEventsId)
-      displayManager =
-          registrar.activity()!!.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
-      val displayConnectedStreamHandler = DisplayConnectedStreamHandler(displayManager)
-      eventChannel.setStreamHandler(displayConnectedStreamHandler)
-    }
+//    /** @hide */
+//    @Suppress("unused", "DEPRECATION")
+//    @JvmStatic
+//    fun registerWith(registrar: PluginRegistry.Registrar) {
+//      val channel = MethodChannel(registrar.messenger(), viewTypeId)
+//      channel.setMethodCallHandler(PresentationDisplaysPlugin())
+//
+//      val eventChannel = EventChannel(registrar.messenger(), viewTypeEventsId)
+//      displayManager =
+//          registrar.activity()!!.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+//      val displayConnectedStreamHandler = DisplayConnectedStreamHandler(displayManager)
+//      eventChannel.setStreamHandler(displayConnectedStreamHandler)
+//    }
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
